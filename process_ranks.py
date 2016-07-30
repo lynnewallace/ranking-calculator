cf7b130224fc9f2e038a44aaeffce74c7c7824f4
lynnewallace <lwallace890@gmail.com>
2016-07-30 22:32:55 +0100

    Update data

--- process_ranks.py
+++ process_ranks.py
@@ -1,23 +1,23 @@
 def process_rank_data(text):
     team_data = text.split("\n")[1:]
-    last_team=len(team_data)
+    last_team = len(team_data)
 
     teams_json = open("./js/teams.json", "w")
     teams_json.write("[\n")
 
     for team in team_data:
-        teams_json.write("{\n")
+        teams_json.write("  {\n")
 
         details = team.split("\t")
 
-        teams_json.write('"t_name": "{}",\n'.format(details[1]))
-        teams_json.write('"t_strength": "{}",\n'.format(details[2]))
-        teams_json.write('"t_rank": {}'.format(details[0]))
+        teams_json.write('    "t_name": "{}",\n'.format(details[1]))
+        teams_json.write('    "t_strength": "{}",\n'.format(details[2]))
+        teams_json.write('    "t_rank": {}'.format(details[0]))
 
         if int(details[0]) == last_team:
-            teams_json.write("\n}\n")
+            teams_json.write("\n  }\n")
         else:
-            teams_json.write("\n},\n")
+            teams_json.write("\n  },\n")
 
     teams_json.write("]")
     teams_json.close()
